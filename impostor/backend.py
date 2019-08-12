40b5fffbd4ad3227c2feb4e3d5acc0be928c6b45
Ranju R <ranjur@users.noreply.github.com>
2019-08-12 14:13:29 +0530

    Update backend.py

--- impostor/backend.py
+++ impostor/backend.py
@@ -1,8 +1,10 @@
 import inspect
 
 import django.contrib.auth as auth
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group
 from django.http import HttpRequest
+from django.contrib.auth import get_user_model
+
 from models import ImpostorLog
 
 from django.conf import settings
@@ -11,6 +13,8 @@ try:
     IMPOSTOR_GROUP = Group.objects.get(name=settings.IMPOSTOR_GROUP)
 except:
     IMPOSTOR_GROUP = None
+
+User = get_user_model()
 
 
 def find_request():
@@ -39,48 +43,48 @@ class AuthBackend:
 
     def authenticate(self, username=None, password=None):
         auth_user = None
-        try:
-            # Admin logging as user?
-            admin, uuser = [uname.strip() for uname in username.split(" as ")]
+        # try:
+        # Admin logging as user?
+        admin, uuser = [uname.strip() for uname in username.split(" as ")]
 
-            # Check if admin exists and authenticates
-            admin_obj = User.objects.get(username=admin)
-            if (admin_obj.is_superuser or (IMPOSTOR_GROUP and IMPOSTOR_GROUP in admin_obj.groups.all(
-            ))) and admin_obj.check_password(password):
-                try:
-                    auth_user = User.objects.get(username=uuser)
-                except User.DoesNotExist:
-                    auth_user = User.objects.get(email=uuser)
+        # Check if admin exists and authenticates
+        get_query_with = {User.USERNAME_FIELD: admin}
+        admin_obj = User.objects.get(**get_query_with)
+        if (admin_obj.is_superuser or (IMPOSTOR_GROUP and IMPOSTOR_GROUP in admin_obj.groups.all(
+        ))) and admin_obj.check_password(password):
 
-            if auth_user:
-                # Superusers can only be impersonated by other superusers
-                if auth_user.is_superuser and not admin_obj.is_superuser:
-                    auth_user = None
-                    raise Exception(
-                        "Superuser can only be impersonated by a superuser.")
+            get_query_with = {User.USERNAME_FIELD: uuser}
+            auth_user = User.objects.get(**get_query_with)
 
-                # Try to find request object and maybe be lucky enough to find
-                # IP address there
-                request = find_request()
-                ip_addr = ''
-                if request:
-                    ip_addr = request.META.get(
-                        'HTTP_X_FORWARDED_FOR', request.META.get(
-                            'HTTP_X_REAL_IP', request.META.get(
-                                'REMOTE_ADDR', '')))
-                    # if there are several ip addresses separated by comma
-                    # like HTTP_X_FORWARDED_FOR returns,
-                    # take only the first one, which is the client's address
-                    if ',' in ip_addr:
-                        ip_addr = ip_addr.split(',', 1)[0].strip()
-                log_entry = ImpostorLog.objects.create(
-                    impostor=admin_obj, imposted_as=auth_user, impostor_ip=ip_addr)
+        if auth_user:
+            # Superusers can only be impersonated by other superusers
+            if auth_user.is_superuser and not admin_obj.is_superuser:
+                auth_user = None
+                raise Exception(
+                    "Superuser can only be impersonated by a superuser.")
 
-                if log_entry.token and request:
-                    request.session['impostor_token'] = log_entry.token
+            # Try to find request object and maybe be lucky enough to find
+            # IP address there
+            request = find_request()
+            ip_addr = ''
+            if request:
+                ip_addr = request.META.get(
+                    'HTTP_X_FORWARDED_FOR', request.META.get(
+                        'HTTP_X_REAL_IP', request.META.get(
+                            'REMOTE_ADDR', '')))
+                # if there are several ip addresses separated by comma
+                # like HTTP_X_FORWARDED_FOR returns,
+                # take only the first one, which is the client's address
+                if ',' in ip_addr:
+                    ip_addr = ip_addr.split(',', 1)[0].strip()
+            log_entry = ImpostorLog.objects.create(
+                impostor=admin_obj, imposted_as=auth_user, impostor_ip=ip_addr)
 
-        except:  # Nope. Do nothing and let other backends handle it.
-            pass
+            if log_entry.token and request:
+                request.session['impostor_token'] = log_entry.token
+
+        # except:  # Nope. Do nothing and let other backends handle it.
+        #     pass
         return auth_user
 
     def get_user(self, user_id):
